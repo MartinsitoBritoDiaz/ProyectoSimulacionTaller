c8966079e992e360a5a1254c155dc50f6f033109
Martinsito Brito Diaz <martinsito_brito@ucne.edu.do>
2021-03-23 18:56:33 -0400

    Cambios

--- ProyectoSimulacionTaller/Form1.Designer.cs
+++ ProyectoSimulacionTaller/Form1.Designer.cs
@@ -30,12 +30,237 @@ namespace ProyectoSimulacionTaller
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Marcos4Label = new System.Windows.Forms.Label();
+            this.Marcos6Label = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Minutoslabel = new System.Windows.Forms.Label();
+            this.Horaslabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(422, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(104, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(422, 273);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(104, 92);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Marcos 4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Marcos 6";
+            // 
+            // Marcos4Label
+            // 
+            this.Marcos4Label.AutoSize = true;
+            this.Marcos4Label.Location = new System.Drawing.Point(117, 63);
+            this.Marcos4Label.Name = "Marcos4Label";
+            this.Marcos4Label.Size = new System.Drawing.Size(28, 17);
+            this.Marcos4Label.TabIndex = 4;
+            this.Marcos4Label.Text = ".....";
+            // 
+            // Marcos6Label
+            // 
+            this.Marcos6Label.AutoSize = true;
+            this.Marcos6Label.Location = new System.Drawing.Point(118, 137);
+            this.Marcos6Label.Name = "Marcos6Label";
+            this.Marcos6Label.Size = new System.Drawing.Size(28, 17);
+            this.Marcos6Label.TabIndex = 5;
+            this.Marcos6Label.Text = ".....";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.Minutoslabel);
+            this.groupBox1.Controls.Add(this.Horaslabel);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(762, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 49);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 29);
+            this.label7.TabIndex = 11;
+            this.label7.Text = ":";
+            // 
+            // Minutoslabel
+            // 
+            this.Minutoslabel.AutoSize = true;
+            this.Minutoslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minutoslabel.Location = new System.Drawing.Point(54, 16);
+            this.Minutoslabel.Name = "Minutoslabel";
+            this.Minutoslabel.Size = new System.Drawing.Size(34, 25);
+            this.Minutoslabel.TabIndex = 9;
+            this.Minutoslabel.Text = "00";
+            this.Minutoslabel.Click += new System.EventHandler(this.Minutoslabel_Click);
+            // 
+            // Horaslabel
+            // 
+            this.Horaslabel.AutoSize = true;
+            this.Horaslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Horaslabel.Location = new System.Drawing.Point(10, 16);
+            this.Horaslabel.Name = "Horaslabel";
+            this.Horaslabel.Size = new System.Drawing.Size(34, 25);
+            this.Horaslabel.TabIndex = 7;
+            this.Horaslabel.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "....";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "....";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Almacen";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(117, 234);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 17);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Llegada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Total";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(201, 317);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(874, 497);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Marcos6Label);
+            this.Controls.Add(this.Marcos4Label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Marcos4Label;
+        private System.Windows.Forms.Label Marcos6Label;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label Minutoslabel;
+        private System.Windows.Forms.Label Horaslabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- ProyectoSimulacionTaller/Form1.cs
+++ ProyectoSimulacionTaller/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,359 @@ namespace ProyectoSimulacionTaller
 {
     public partial class Form1 : Form
     {
+        public int Horas = 0;
+        public int Minutos = 0;
+        public int Segundos = 0;
+
+        public int CantMarcos4Taller = 0;
+        public int CantMarcos6Taller = 0;
+        
+        
+        public int Contador = 0;
+
+        public int HoraActualC1 = 0;
+        public int HoraActualC2 = 0;
+        public int HoraActualC3 = 0;
+        public int HoraActualC4 = 0;
+        public int HoraActualC5 = 0;
+
+        bool pasoC1 = false;
+        bool pasoC2 = false;
+        bool pasoC3 = false;
+        bool pasoC4 = false;
+        bool pasoC5 = false;
+
+        Queue ColaLlegada = new Queue();
+
+        List<int> ListaAlmacen = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public double Probabilidad()
+        {
+            Random auxiliar = new Random();
+            double probabilidad;
+
+            probabilidad = auxiliar.NextDouble();
+
+            probabilidad *= 100;
+
+            return probabilidad;
+        }
+
+        public void Llegada()
+        {
+            Minutos++; 
+
+            double llegada = Probabilidad();
+            
+            if(Minutos == 60)
+            {
+                Horas++;
+
+                if(Horas == 24)
+                {
+                    Horas = 00;
+                    Minutos = 00;
+                }
+
+
+                Minutos = 00;
+            }
+
+            if((Horas % 5 == 0) && (Minutos == 1))
+            {
+                int aux = 0;
+
+                if ((llegada >= 0) && (llegada <= 60))
+                {
+                    aux = 4;
+                    CantMarcos4Taller += aux;
+                    Marcos4Label.Text = Convert.ToString(CantMarcos4Taller);
+                }
+                else
+                {
+                    if ((llegada > 60) && (llegada <= 100))
+                    {
+                        aux = 6;
+                        CantMarcos6Taller += aux;
+                        Marcos6Label.Text = Convert.ToString(CantMarcos6Taller);
+                    }
+                }
+
+
+                label9.Text = Convert.ToString(CantMarcos4Taller + CantMarcos6Taller);
+                AsignarMarcosLleganAEnsamble(aux);
+            }
+
+
+            Ensambalar();
+
+            if (Minutos < 10)
+                Minutoslabel.Text = Convert.ToString("0" + Minutos);
+            else
+                Minutoslabel.Text = Convert.ToString(Minutos);
+
+
+            if (Horas < 10)
+                Horaslabel.Text = Convert.ToString("0" + Horas);
+            else
+                Horaslabel.Text = Convert.ToString(Horas);
+
+        }
+
+
+        public void AsignarMarcosLleganAEnsamble(int aux)
+        {
+            //Asignacion de marcos Desmontados
+            if (aux == 4)
+            {
+                ColaLlegada.Enqueue(Contador);
+
+                Contador++;
+
+                ColaLlegada.Enqueue(Contador);
+
+                Contador++;
+
+                ColaLlegada.Enqueue(Contador);
+
+                Contador++;
+
+                ColaLlegada.Enqueue(Contador);
+            }
+            else
+            {
+                if (aux == 6)
+                {
+                    ColaLlegada.Enqueue(Contador);
+
+                    Contador++;
+
+                    ColaLlegada.Enqueue(Contador);
+
+                    Contador++;
+
+                    ColaLlegada.Enqueue(Contador);
+
+                    Contador++;
+
+                    ColaLlegada.Enqueue(Contador);
+
+                    Contador++;
+
+                    ColaLlegada.Enqueue(Contador);
+
+                    Contador++;
+
+                    ColaLlegada.Enqueue(Contador);
+                }
+            }
+        }
+
+        public void Ensambalar()
+        {
+
+            if(Carpintero1() != -1)
+            {
+                ListaAlmacen.Add(Carpintero1());
+            }
+            
+            if(Carpintero2() != -1)
+            {
+                ListaAlmacen.Add(Carpintero2());
+            }
+            
+            if(Carpintero3() != -1)
+            {
+                ListaAlmacen.Add(Carpintero3());
+            }
+            
+            if(Carpintero4() != -1)
+            {
+                ListaAlmacen.Add(Carpintero4());
+            }
+            
+            if(Carpintero5() != -1)
+            {
+                ListaAlmacen.Add(Carpintero5());
+            }
+
+            label4.Text = Convert.ToString(ListaAlmacen.Count());
+        }
+
+        public int Carpintero1()
+        {
+
+            if(ColaLlegada.Count > 0)
+            {
+
+                if (pasoC1)
+                {
+                    HoraActualC1 = Horas;
+                    pasoC1 = false;
+                    return (int)ColaLlegada.Dequeue();
+                }
+
+                if (!pasoC1)
+                {
+                    if ((Horas > HoraActualC1) && (HoraActualC1 + 2 == Horas))
+                        pasoC1 = true;
+
+                    if (HoraActualC1 == 22)
+                        if (Horas == 0)
+                            pasoC1 = true;
+
+                    if (HoraActualC1 == 23)
+                        if (Horas == 1)
+                            pasoC1 = true;
+
+                }
+
+
+                label5.Text = Convert.ToString((int)ColaLlegada.Count);
+
+            }
+
+            return -1;
+
+        }
+        public int Carpintero2()
+        {
+            if (ColaLlegada.Count > 0)
+            {
+                if (pasoC2)
+                {
+                    HoraActualC2 = Horas;
+                    pasoC2 = false;
+                    return (int)ColaLlegada.Dequeue();
+                }
+
+                if (!pasoC2)
+                {
+                    if ((Horas > HoraActualC2) && (HoraActualC2 + 3 == Horas))
+                        pasoC2 = true;
+
+                    if (HoraActualC2 == 21)
+                        if (Horas == 0)
+                            pasoC2 = true;
+
+                    if (HoraActualC2 == 22)
+                        if (Horas == 1)
+                            pasoC2 = true;
+                }
+            }
+            return -1;
+        }
+        public int Carpintero3()
+        {
+            if (ColaLlegada.Count > 0)
+            {
+                if (pasoC3)
+                {
+                    HoraActualC3 = Horas;
+                    pasoC3 = false;
+                    return (int)ColaLlegada.Dequeue();
+                }
+
+                if (!pasoC3)
+                {
+                    if ((Horas > HoraActualC3) && (HoraActualC3 + 4 == Horas))
+                        pasoC3 = true;
+
+                    if (HoraActualC3 == 21)
+                        if (Horas == 0)
+                            pasoC3 = true;
+
+                    if (HoraActualC3 == 22)
+                        if (Horas == 1)
+                            pasoC3 = true;
+                }
+            }
+            return -1;
+        }
+        public int Carpintero4()
+        {
+            if (ColaLlegada.Count > 0)
+            {
+
+                if (pasoC4)
+                {
+                    HoraActualC4 = Horas;
+                    pasoC4 = false;
+                    return (int)ColaLlegada.Dequeue();
+                }
+
+                if (!pasoC4)
+                {
+                    if ((Horas > HoraActualC4) && (HoraActualC4 + 5 == Horas))
+                        pasoC4 = true;
+
+                    if (HoraActualC4 == 20)
+                        if (Horas == 0)
+                            pasoC4 = true;
+
+                    if (HoraActualC4 == 21)
+                        if (Horas == 1)
+                            pasoC4 = true;
+                }
+            }
+            return -1;
+        }
+        public int Carpintero5()
+        {
+            if (ColaLlegada.Count > 0)
+            {
+
+                if (pasoC5)
+                {
+                    HoraActualC5 = Horas;
+                    pasoC5 = false;
+                    return (int)ColaLlegada.Dequeue();
+                }
+
+                if (!pasoC5)
+                {
+                    if ((Horas > HoraActualC5) && (HoraActualC5 + 6 == Horas))
+                        pasoC5 = true;
+
+                    if (HoraActualC5 == 19)
+                        if (Horas == 0)
+                            pasoC5 = true;
+
+                    if (HoraActualC5 == 20)
+                        if (Horas == 1)
+                            pasoC5 = true;
+                }
+            }
+            return -1;
+        }
+        
+        public void Almnacen()
+        {
+
+        }
+        public void Ejecutar()
+        {
+            Llegada();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Ejecutar();
+        }
+
+        private void Minutoslabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
