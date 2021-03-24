7fdffeb5dfadc3366631f8326a1cfb79467d009f
Jeferson Raul Ortega Brito <jeferson_ortega@ucne.edu.do>
2021-03-24 12:36:13 -0400

    avances

--- ProyectoSimulacionTaller/Form1.Designer.cs
+++ ProyectoSimulacionTaller/Form1.Designer.cs
@@ -52,6 +52,10 @@ namespace ProyectoSimulacionTaller
             this.label5 = new System.Windows.Forms.Label();
             this.Pinturalabel = new System.Windows.Forms.Label();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            this.Empaquelabel = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Despachadoslabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -66,10 +70,10 @@ namespace ProyectoSimulacionTaller
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(316, 129);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(604, 176);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(78, 75);
+            this.pictureBox1.Size = new System.Drawing.Size(104, 92);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -77,10 +81,10 @@ namespace ProyectoSimulacionTaller
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(316, 222);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Location = new System.Drawing.Point(604, 290);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(78, 75);
+            this.pictureBox2.Size = new System.Drawing.Size(104, 92);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -88,30 +92,27 @@ namespace ProyectoSimulacionTaller
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 26);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(47, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(66, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Marcos 4";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 80);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(47, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.Size = new System.Drawing.Size(66, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Marcos 6";
             // 
             // Marcos4Label
             // 
             this.Marcos4Label.AutoSize = true;
-            this.Marcos4Label.Location = new System.Drawing.Point(88, 51);
-            this.Marcos4Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Marcos4Label.Location = new System.Drawing.Point(117, 63);
             this.Marcos4Label.Name = "Marcos4Label";
-            this.Marcos4Label.Size = new System.Drawing.Size(22, 13);
+            this.Marcos4Label.Size = new System.Drawing.Size(28, 17);
             this.Marcos4Label.TabIndex = 4;
             this.Marcos4Label.Text = ".....";
             this.Marcos4Label.Click += new System.EventHandler(this.Marcos4Label_Click);
@@ -119,10 +120,9 @@ namespace ProyectoSimulacionTaller
             // Marcos6Label
             // 
             this.Marcos6Label.AutoSize = true;
-            this.Marcos6Label.Location = new System.Drawing.Point(88, 111);
-            this.Marcos6Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Marcos6Label.Location = new System.Drawing.Point(117, 137);
             this.Marcos6Label.Name = "Marcos6Label";
-            this.Marcos6Label.Size = new System.Drawing.Size(22, 13);
+            this.Marcos6Label.Size = new System.Drawing.Size(28, 17);
             this.Marcos6Label.TabIndex = 5;
             this.Marcos6Label.Text = ".....";
             // 
@@ -132,11 +132,11 @@ namespace ProyectoSimulacionTaller
             this.groupBox1.Controls.Add(this.Minutoslabel);
             this.groupBox1.Controls.Add(this.Horaslabel);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(538, 10);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Location = new System.Drawing.Point(717, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(109, 40);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(145, 49);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
@@ -144,10 +144,9 @@ namespace ProyectoSimulacionTaller
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(42, 11);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(56, 14);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(15, 24);
+            this.label7.Size = new System.Drawing.Size(19, 29);
             this.label7.TabIndex = 11;
             this.label7.Text = ":";
             // 
@@ -155,10 +154,9 @@ namespace ProyectoSimulacionTaller
             // 
             this.Minutoslabel.AutoSize = true;
             this.Minutoslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Minutoslabel.Location = new System.Drawing.Point(61, 13);
-            this.Minutoslabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Minutoslabel.Location = new System.Drawing.Point(81, 16);
             this.Minutoslabel.Name = "Minutoslabel";
-            this.Minutoslabel.Size = new System.Drawing.Size(27, 20);
+            this.Minutoslabel.Size = new System.Drawing.Size(34, 25);
             this.Minutoslabel.TabIndex = 9;
             this.Minutoslabel.Text = "00";
             this.Minutoslabel.Click += new System.EventHandler(this.Minutoslabel_Click);
@@ -167,99 +165,89 @@ namespace ProyectoSimulacionTaller
             // 
             this.Horaslabel.AutoSize = true;
             this.Horaslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Horaslabel.Location = new System.Drawing.Point(11, 13);
-            this.Horaslabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Horaslabel.Location = new System.Drawing.Point(15, 16);
             this.Horaslabel.Name = "Horaslabel";
-            this.Horaslabel.Size = new System.Drawing.Size(27, 20);
+            this.Horaslabel.Size = new System.Drawing.Size(34, 25);
             this.Horaslabel.TabIndex = 7;
             this.Horaslabel.Text = "00";
             // 
             // Almacenlabel
             // 
             this.Almacenlabel.AutoSize = true;
-            this.Almacenlabel.Location = new System.Drawing.Point(151, 222);
-            this.Almacenlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Almacenlabel.Location = new System.Drawing.Point(201, 273);
             this.Almacenlabel.Name = "Almacenlabel";
-            this.Almacenlabel.Size = new System.Drawing.Size(19, 13);
+            this.Almacenlabel.Size = new System.Drawing.Size(24, 17);
             this.Almacenlabel.TabIndex = 8;
             this.Almacenlabel.Text = "....";
             // 
             // Ensamblajelabe
             // 
             this.Ensamblajelabe.AutoSize = true;
-            this.Ensamblajelabe.Location = new System.Drawing.Point(151, 190);
-            this.Ensamblajelabe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Ensamblajelabe.Location = new System.Drawing.Point(201, 234);
             this.Ensamblajelabe.Name = "Ensamblajelabe";
-            this.Ensamblajelabe.Size = new System.Drawing.Size(19, 13);
+            this.Ensamblajelabe.Size = new System.Drawing.Size(24, 17);
             this.Ensamblajelabe.TabIndex = 9;
             this.Ensamblajelabe.Text = "....";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(75, 222);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(100, 273);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.Size = new System.Drawing.Size(62, 17);
             this.label6.TabIndex = 10;
             this.label6.Text = "Almacen";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(75, 190);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(100, 234);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(61, 13);
+            this.label8.Size = new System.Drawing.Size(81, 17);
             this.label8.TabIndex = 11;
             this.label8.Text = "Ensamblaje";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(75, 284);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(100, 410);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.Size = new System.Drawing.Size(40, 17);
             this.label3.TabIndex = 12;
             this.label3.Text = "Total";
             // 
             // Totallabel
             // 
             this.Totallabel.AutoSize = true;
-            this.Totallabel.Location = new System.Drawing.Point(151, 284);
-            this.Totallabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Totallabel.Location = new System.Drawing.Point(201, 410);
             this.Totallabel.Name = "Totallabel";
-            this.Totallabel.Size = new System.Drawing.Size(19, 13);
+            this.Totallabel.Size = new System.Drawing.Size(24, 17);
             this.Totallabel.TabIndex = 13;
             this.Totallabel.Text = "....";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(200, 353);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(267, 434);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.Size = new System.Drawing.Size(0, 17);
             this.label4.TabIndex = 14;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(75, 253);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(100, 311);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(53, 17);
             this.label5.TabIndex = 16;
             this.label5.Text = "Pintura";
             // 
             // Pinturalabel
             // 
             this.Pinturalabel.AutoSize = true;
-            this.Pinturalabel.Location = new System.Drawing.Point(151, 253);
-            this.Pinturalabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Pinturalabel.Location = new System.Drawing.Point(201, 311);
             this.Pinturalabel.Name = "Pinturalabel";
-            this.Pinturalabel.Size = new System.Drawing.Size(19, 13);
+            this.Pinturalabel.Size = new System.Drawing.Size(24, 17);
             this.Pinturalabel.TabIndex = 15;
             this.Pinturalabel.Text = "....";
             // 
@@ -267,11 +255,51 @@ namespace ProyectoSimulacionTaller
             // 
             this.timer2.Interval = 25;
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(100, 346);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 17);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Empaque";
+            // 
+            // Empaquelabel
+            // 
+            this.Empaquelabel.AutoSize = true;
+            this.Empaquelabel.Location = new System.Drawing.Point(201, 346);
+            this.Empaquelabel.Name = "Empaquelabel";
+            this.Empaquelabel.Size = new System.Drawing.Size(24, 17);
+            this.Empaquelabel.TabIndex = 17;
+            this.Empaquelabel.Text = "....";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(100, 380);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(95, 17);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Despachados";
+            // 
+            // Despachadoslabel
+            // 
+            this.Despachadoslabel.AutoSize = true;
+            this.Despachadoslabel.Location = new System.Drawing.Point(201, 380);
+            this.Despachadoslabel.Name = "Despachadoslabel";
+            this.Despachadoslabel.Size = new System.Drawing.Size(24, 17);
+            this.Despachadoslabel.TabIndex = 19;
+            this.Despachadoslabel.Text = "....";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(656, 404);
+            this.ClientSize = new System.Drawing.Size(875, 497);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Despachadoslabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Empaquelabel);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.Pinturalabel);
             this.Controls.Add(this.label4);
@@ -288,7 +316,7 @@ namespace ProyectoSimulacionTaller
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -324,6 +352,10 @@ namespace ProyectoSimulacionTaller
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label Pinturalabel;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label Empaquelabel;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label Despachadoslabel;
     }
 }
 
--- ProyectoSimulacionTaller/Form1.cs
+++ ProyectoSimulacionTaller/Form1.cs
@@ -29,6 +29,8 @@ namespace ProyectoSimulacionTaller
         public int HoraActualC4 = 0;
         public int HoraActualC5 = 0;
 
+        public int total =0;
+
         bool pasoC1 = false;
         bool pasoC2 = false;
         bool pasoC3 = false;
@@ -38,7 +40,12 @@ namespace ProyectoSimulacionTaller
         Queue ColaLlegada = new Queue();
 
         List<int> ListaAlmacen = new List<int>();
+        List<int> ListaHoraEntradaAlmacen = new List<int>();
+
         List<int> ListaPintado = new List<int>();
+        Queue ColaEmpaque = new Queue();
+
+        int TiempoMantenimiento = 0;
 
         public Form1()
         {
@@ -90,6 +97,7 @@ namespace ProyectoSimulacionTaller
                 {
                     aux = 4;
                     CantMarcos4Taller ++;
+                    
                     Marcos4Label.Text = Convert.ToString(CantMarcos4Taller);
                 }
                 else
@@ -101,9 +109,11 @@ namespace ProyectoSimulacionTaller
                         Marcos6Label.Text = Convert.ToString(CantMarcos6Taller);
                     }
                 }
+                total += aux;
 
 
-                Totallabel.Text = Convert.ToString(CantMarcos4Taller*(4) + CantMarcos6Taller*(6));
+                //Totallabel.Text = Convert.ToString(CantMarcos4Taller*(4) + CantMarcos6Taller*(6));
+                Totallabel.Text = Convert.ToString(aux);
                 AsignarMarcosLleganAEnsamble(aux);
             }
 
@@ -122,42 +132,13 @@ namespace ProyectoSimulacionTaller
                 Horaslabel.Text = Convert.ToString(Horas);
 
             Almacen();
-
+            ProcesoPintura();
+            Empaque();
             
         }
 
     
-        List<int> ListaHoraEntradaAlmacen = new List<int>();
-        public void Almacen()
-        {
-            
-                if (ListaAlmacen.Count > 1)
-                {
-                  /* foreach (var horaLlegada in ListaEntradaAlmacen)
-                    {
-                        if (Horas == horaLlegada)
-                        {
-                            ListaPintado.Add(1);
-                            aux = ListaEntradaAlmacen.IndexOf(horaLlegada);
-                            
-                        }
-
-                    }*/
-
-                    if (ListaHoraEntradaAlmacen.First() == Horas)
-                    {
-                        ListaPintado.Add(1);
-                        ListaHoraEntradaAlmacen.Remove(Horas);
-                        ListaAlmacen.RemoveAt(0);
-                        
-                    }
-                }
-
-                Pinturalabel.Text = ListaPintado.Count().ToString();
-
-            
-
-        }
+        
 
         public void AsignarMarcosLleganAEnsamble(int aux)
         {
@@ -389,10 +370,131 @@ namespace ProyectoSimulacionTaller
             }
             return -1;
         }
-        
-        
-        
 
+
+        public void Almacen()
+        {
+
+            if (ListaAlmacen.Count > 1)
+            {
+                /* foreach (var horaLlegada in ListaEntradaAlmacen)
+                  {
+                      if (Horas == horaLlegada)
+                      {
+                          ListaPintado.Add(1);
+                          aux = ListaEntradaAlmacen.IndexOf(horaLlegada);
+
+                      }
+
+                  }*/
+
+                if (ListaHoraEntradaAlmacen.First() == Horas)
+                {
+                    ListaPintado.Add(1);
+                    ListaHoraEntradaAlmacen.Remove(Horas);
+                    ListaAlmacen.RemoveAt(0);
+
+                }
+            }
+
+            Pinturalabel.Text = ListaPintado.Count().ToString();
+
+        }
+
+        int TiempoPintando = 0;
+        int aux2Pintura = 0; //auxiliar para identificar si va a se 10, 11, 12 ... 20 minutos
+        public void ProcesoPintura()
+        {
+            if((total%20)!=0)
+            if (ListaPintado.Count > 0)
+            {
+                TiempoPintando++; //Tiempo que va transcurriendo pintando 1
+
+                if (TiempoPintando == (aux2Pintura+10)) //si el tiempo alcanzo lo requerido
+                {
+                    TiempoPintando = 0;
+                    
+
+                    if (aux2Pintura == 10)
+                        aux2Pintura = 0;
+
+                    double inspeccion = Probabilidad();
+
+                    if(inspeccion>=0 && inspeccion < 90)
+                    {
+                        ColaEmpaque.Enqueue(1);
+                        ListaPintado.RemoveAt(0);
+                        aux2Pintura++; //para saber si va en el 1, 2....
+                    }
+                    else
+                        if(inspeccion>90 && inspeccion <= 100)
+                        {
+                            timer1.Stop();
+                            MessageBox.Show("Este marco no ha superado con éxito la inspeccion, debe ser retrabajado", "Aviso!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            timer1.Start();
+                            ListaPintado.RemoveAt(0);
+                            ListaPintado.Add(1);
+                            aux2Pintura++; //para saber si va en el 1, 2....
+                        }
+
+                }
+            }
+            
+
+            Empaquelabel.Text = Convert.ToString((int)ColaEmpaque.Count);
+        }
+
+        int Despachado = 0;
+        int TiempoEmpacando = 0;
+        int AuxEmpaque = 0;
+        public void Empaque()
+        {
+            if (ColaEmpaque.Count > 0)
+            {
+                TiempoEmpacando++;
+
+                if (TiempoEmpacando == (AuxEmpaque + 10))
+                {
+                    if (AuxEmpaque == 5)
+                        AuxEmpaque = 0;
+
+                    TiempoEmpacando = 0;
+
+                    Despachado++;
+                    ColaEmpaque.Dequeue();
+                }
+            }
+
+            Despachadoslabel.Text = Despachado.ToString();
+        }
+
+        public bool Mantenimiento()
+        {
+            double aux = Probabilidad();
+            bool EnMantenimiento = false;
+            if ((total % 20)==0)
+            {
+                if(aux>=0 && aux < (1 / 3 *100))
+                {
+                    TiempoMantenimiento = 30;
+                    EnMantenimiento = true;
+                }else
+                    if(aux>(1/3*100) && aux < (1 / 2 * 100)) 
+                    { 
+                        TiempoMantenimiento=45;
+                        EnMantenimiento = true;
+                    }
+                    else
+                        if(aux>(1/2*100) && aux <= (100)) 
+                        {
+                            TiempoMantenimiento = 60;
+                            EnMantenimiento = true;
+                        }
+                            
+            }
+
+            return EnMantenimiento;
+        }
 
         public void Ejecutar()
         {
